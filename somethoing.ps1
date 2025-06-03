84759822c163e46fd5775abc462263d95965d86e
PeanutButter69 <109911533+PeanutButter6996@users.noreply.github.com>
2025-06-03 17:09:12 +0700

    Update somethoing.ps1

--- somethoing.ps1
+++ somethoing.ps1
@@ -1,2 +1,3 @@
-$keys = $valuelog.Keys | Where-Object { $valuelog[$_] -is [NaObject] -or "$($valuelog[$_])".Contains('System.Management.Automation.Deobfuscation.NaNObject') }
+$keys = $valuelog.Keys | Where-Object { $valuelog[$_] -is [NaObject] -or "$($valuelog[$_])"
+                                            .Contains('System.Management.Automation.Deobfuscation.NaNObject') }
 foreach ($key in $keys) { $valuelog[$key] = $null; }
