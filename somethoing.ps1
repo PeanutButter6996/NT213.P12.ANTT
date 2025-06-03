33177209080820b645bc74a650fd31003950251d
PeanutButter69 <109911533+PeanutButter6996@users.noreply.github.com>
2025-06-03 17:08:49 +0700

    Create somethoing.ps1

---
+++ somethoing.ps1
@@ -0,0 +1,2 @@
+$keys = $valuelog.Keys | Where-Object { $valuelog[$_] -is [NaObject] -or "$($valuelog[$_])".Contains('System.Management.Automation.Deobfuscation.NaNObject') }
+foreach ($key in $keys) { $valuelog[$key] = $null; }
