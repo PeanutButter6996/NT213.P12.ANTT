a80c95aa7fa0b16daf292bf56e9941c894095fa3
PeanutButter69 <109911533+PeanutButter6996@users.noreply.github.com>
2025-06-08 11:28:53 +0700

    Update somethoing.ps1

--- somethoing.ps1
+++ somethoing.ps1
@@ -1,11 +1,26 @@
-function Stringify ($Object, $check = $true) {
-    #$global:Object2 = $Object
-    if ($check -and $null -ne $Object -and $null -ne $global:Rule['ALLOW_TYPE'] 
-                    -and -not $global:Rule['ALLOW_TYPE'].Contains($Object.GetType())) { throw 'Type denied!' }
-    if ($check -and $null -ne $Object -and ($global:Rule['BAN_TYPE'].Contains($Object.GetType()) 
-                    -or @('RuntimeAssembly') -contains $Object.GetType().Name)) { throw 'Type denied!' }
-    if ($global:Rule['STRONG_TYPE']) { $t = (../Deobfuscation/ConvertTo-Expression.ps1 $Object $check -Strong -Expand -1 -Depth 3); }
-    else { $t = (../Deobfuscation/ConvertTo-Expression.ps1 $Object $check -Expand -1 -Depth 3); }
-    if ($check -and $t -match '\[pscustomobject\]') { throw 'pscustomobject denied!' }
-    return $t
+function CodeFormat ($ScriptString) {
+    $tokens = [System.Management.Automation.PSParser]::Tokenize($ScriptString, [ref]$null)
+    $tokens = $tokens | Sort-Object { - $_.Start }
+    $lineflag = $false
+    $linestart = 0
+    $lineend = 0
+    $groupdepth = 0
+    $identtype = '    '
+    for ($i = 0; $i -lt $tokens.Count; $i++) {
+        $token = $tokens[$i]
+        if ($token.Type -eq 'GroupEnd') { $groupdepth ++ }
+        if ($token.Type -eq 'GroupStart') { $groupdepth -- }
+        if (($token.Type -eq 'StatementSeparator' -and $groupdepth -eq 0) -or ($token.Type -eq 'NewLine')) {
+            if (!$lineflag) { $lineend = $token.Start + $token.Length; $lineflag = $true; }
+        } elseif ($lineflag) {
+            $lineflag = $false
+            $linestart = $token.Start + $token.Length
+            $ScriptString = $ScriptString.SubString(0, $linestart) + "`r`n" + $ScriptString.SubString($lineend)
+        }
+        if ($token.Type -eq 'Command' -and $tokens[$i + 1] -and @('NewLine', 'LineContinuation').Contains($tokens[$i + 1].Type.ToString())) {
+            $ScriptString = $ScriptString.SubString(0, $tokens[$i + 1].Start + $tokens[$i + 1].Length) + 
+                                                            $identtype * $groupdepth + $ScriptString.SubString($token.Start)
+        }
+    }
+    return $ScriptString
 }
