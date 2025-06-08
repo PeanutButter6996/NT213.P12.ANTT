ecb79dff08b92be070b9f62fe2f3e47983de78cf
PeanutButter69 <109911533+PeanutButter6996@users.noreply.github.com>
2025-06-08 10:09:19 +0700

    Update somethoing.ps1

--- somethoing.ps1
+++ somethoing.ps1
@@ -1,3 +1,14 @@
-$keys = $valuelog.Keys | Where-Object { $valuelog[$_] -is [NaObject] -or "$($valuelog[$_])"
-                                            .Contains('System.Management.Automation.Deobfuscation.NaNObject') }
-foreach ($key in $keys) { $valuelog[$key] = $null; }
+function PostTraversal ($curNode) {
+    $curId = (GetHashCode $curNode)
+    if ( $curNode.GetType().Name -eq 'PipelineAst' -and $valuelog[$iexPrefix + $curNode.PipelineElements[-1].Extent.StartOffset + ',' 
+          + $curNode.PipelineElements[-1].Extent.EndOffset] -eq 'Foreach-Object' -and `
+            $curNode.PipelineElements[-1].CommandElements[-1].GetType().Name -eq 'ScriptBlockExpressionAst') {
+        $count = $foreachCount[$iexPrefix + $curNode.PipelineElements[-1].Extent.StartOffset + ',' + $curNode.PipelineElements[-1].Extent.EndOffset]
+        $BlockString = $NodeString[(GetHashCode $curNode.PipelineElements[-1].CommandElements[-1].ScriptBlock.EndBlock)]
+        if ($count -eq $hookTimes -and -not $BlockString.Contains('$_')) { $NodeString[$curId] = $BlockString }
+    }
+    if ( $curNode.GetType().Name -eq 'FunctionDefinitionAst' ) {
+        $funcDeob[$curNode.Name] = $NodeString[$curId]
+        $NodeString[$curId] = $funcDef[$curNode.Name]
+    }
+}
