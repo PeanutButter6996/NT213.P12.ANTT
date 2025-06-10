5dbe56b8834e8e41550bc5224a1cc3e6ec2ea555
PeanutButter69 <109911533+PeanutButter6996@users.noreply.github.com>
2025-06-10 10:07:44 +0700

    Update somethoing.ps1

--- somethoing.ps1
+++ somethoing.ps1
@@ -1,26 +1,8 @@
-function CodeFormat ($ScriptString) {
-    $tokens = [System.Management.Automation.PSParser]::Tokenize($ScriptString, [ref]$null)
-    $tokens = $tokens | Sort-Object { - $_.Start }
-    $lineflag = $false
-    $linestart = 0
-    $lineend = 0
-    $groupdepth = 0
-    $identtype = '    '
-    for ($i = 0; $i -lt $tokens.Count; $i++) {
-        $token = $tokens[$i]
-        if ($token.Type -eq 'GroupEnd') { $groupdepth ++ }
-        if ($token.Type -eq 'GroupStart') { $groupdepth -- }
-        if (($token.Type -eq 'StatementSeparator' -and $groupdepth -eq 0) -or ($token.Type -eq 'NewLine')) {
-            if (!$lineflag) { $lineend = $token.Start + $token.Length; $lineflag = $true; }
-        } elseif ($lineflag) {
-            $lineflag = $false
-            $linestart = $token.Start + $token.Length
-            $ScriptString = $ScriptString.SubString(0, $linestart) + "`r`n" + $ScriptString.SubString($lineend)
-        }
-        if ($token.Type -eq 'Command' -and $tokens[$i + 1] -and @('NewLine', 'LineContinuation').Contains($tokens[$i + 1].Type.ToString())) {
-            $ScriptString = $ScriptString.SubString(0, $tokens[$i + 1].Start + $tokens[$i + 1].Length) + 
-                                                            $identtype * $groupdepth + $ScriptString.SubString($token.Start)
-        }
-    }
-    return $ScriptString
-}
+ try {
+        if ($valuelog[$key] -and ([System.Management.Automation.PSSerializer]::Serialize($valuelog[$key])) -ne 
+                            ([System.Management.Automation.PSSerializer]::Serialize($info))) { $valuelog[$key] = $NaObject; }
+        else { $valuelog[$key] = $info; }
+    } catch {
+        if ($valuelog[$key] -and (Stringify $valuelog[$key] $false) -ne (Stringify $info $false)) { $valuelog[$key] = $NaObject; }
+        else { $valuelog[$key] = $info; }
+    } 
