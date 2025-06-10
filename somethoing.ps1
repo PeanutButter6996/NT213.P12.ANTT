b86e4004f44e4a854da520bed0d26b83425ee88a
PeanutButter69 <109911533+PeanutButter6996@users.noreply.github.com>
2025-06-10 10:07:57 +0700

    Update somethoing.ps1

--- somethoing.ps1
+++ somethoing.ps1
@@ -1,4 +1,4 @@
- try {
+    try {
         if ($valuelog[$key] -and ([System.Management.Automation.PSSerializer]::Serialize($valuelog[$key])) -ne 
                             ([System.Management.Automation.PSSerializer]::Serialize($info))) { $valuelog[$key] = $NaObject; }
         else { $valuelog[$key] = $info; }
